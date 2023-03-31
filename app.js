2e1695e903820673d330a0d36b369c2870d49277
tarun Shori <tarunshori801@gmail.com>
2023-04-01 02:41:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config({
+  path: "./config/config.env",
+});
+const express = require("express");
+const app = express();
+require("./db/conn");
+const router = require("./routes/router");
+const cors = require("cors");
+const cookiParser = require("cookie-parser");
+const port = 8009;
+
+// app.get("/",(req,res)=>{
+//     res.status(201).json("server created")
+// });
+
+app.use(express.json());
+app.use(cookiParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`server start at port no : ${port}`);
+});
